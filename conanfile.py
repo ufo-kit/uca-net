869fd079ca4046790a403f5674c89f55d2acbe4e
Marius Elvert <marius.elvert@softwareschneiderei.de>
2025-06-04 13:54:10 +0200

    Added an option to enable ZMQ support

--- conanfile.py
+++ conanfile.py
@@ -11,20 +11,25 @@ class UcaNetConan(ConanFile):
     description = "TCP-based network bridge for libuca."
     topics = ("utilities",)
     settings = "os", "compiler", "build_type", "arch"
-    options = {"shared": [True, False]}
-    default_options = {"shared": True}
+    options = {"shared": [True, False], "with_zeromq": [True, False]}
+    default_options = {"shared": True, "with_zeromq": False}
     generators = "CMakeDeps"
     exports_sources = "*.h", "*.c", "cmake/*", "CMakeLists.txt", "config.h.in"
-    requires = "libuca/2.3.0", "glib/2.81.0",
     
     def configure(self):
         # These are not applicable for C libraries
         self.settings.rm_safe("compiler.libcxx")
         self.settings.rm_safe("compiler.cppstd")
 
+    def requirements(self):
+        self.requires("libuca/2.3.0")
+        self.requires("glib/2.81.0")
+        if self.options.with_zeromq:
+            self.requires("zeromq/4.3.5")
+
     def generate(self):
         toolchain = CMakeToolchain(self)
-        toolchain.variables["WITH_ZMQ_NETWORKING"] = False # TODO, make this optional
+        toolchain.variables["WITH_ZMQ_NETWORKING"] = (self.options.with_zeromq == True)
         toolchain.variables["USE_FIND_PACKAGE_FOR_GLIB"] = True
         toolchain.generate()
 
